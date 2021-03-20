4c668524896d1f63abab500fdb7c2ec7a4c89ead
corvinus-nmhat5 <abris.agai@stud.uni-corvinus.hu>
2021-03-20 09:47:17 +0100

    jsgyak.html és jsgyak.js létrehozása, 1-től tízig való számok kitétele és Pascal-háromszög megoldása Javascript segítségével

---
+++ HajosTeszt/HajosTeszt/wwwroot/jsgyak.js
@@ -0,0 +1,36 @@
+﻿window.onload = () => {
+    console.log("Oldal betöltve...")
+
+    //dobozok
+    let hova = document.getElementById("szamok");
+    for (var o = 0; o < 10; o++) {
+        let doboz = document.createElement("div");
+        doboz.classList.add("ize");
+        doboz.style.background = `rgb(${o * 25},0,0)`
+        doboz.innerText = (o + 1);
+        hova.appendChild(doboz);
+    }
+
+    //faktoriális
+    var faktoriális = function (n) {
+        let er = 1;
+        for (let i = 2; i <= n; i++) {
+            er = er * i;
+        }
+        return er;
+    }
+
+    //pascal
+    let sz = document.getElementById("pascal");
+    for (var s = 0; s < 10; s++) {
+        let sor = document.createElement("div");
+        sor.classList.add("sor");
+        sz.appendChild(sor);
+        for (var o = 0; o < s; o++) {
+            let e = document.createElement("div");
+            e.classList.add("elem");
+            e.innerHTML = faktoriális(s - 1) / (faktoriális(o) * faktoriális((s - 1) - o));
+            sor.appendChild(e);
+        }
+    }
+}

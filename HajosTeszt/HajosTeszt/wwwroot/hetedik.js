0335fc8bf17aab9050c4ccff1c0a985aa7dba9f7
corvinus-nmhat5 <abris.agai@stud.uni-corvinus.hu>
2021-03-26 10:40:01 +0100

    hajo.html-hez tartozó hetedik.js elkészítése

---
+++ HajosTeszt/HajosTeszt/wwwroot/hetedik.js
@@ -0,0 +1,35 @@
+﻿var kérdések = [];
+var sorszam = 0;
+
+window.onload = () => {
+    letöltés();
+}
+
+kérdésMegjelenítés(sorszam);
+
+//gombok
+document.getElementById("vissza").click = function () {
+    
+}
+document.getElementById("elore").click = function () {
+
+}
+
+//letöltés
+function letöltés() {
+    fetch('/questions.json')
+        .then(response => response.json())
+        .then(data => letöltésBefejeződött(data)
+        );
+
+    function letöltésBefejeződött(d) {
+        console.log("Sikeres letöltés")
+        kérdések = d;
+        console.log(kérdések);
+    }
+}
+
+//kérdés megjelenítés
+function kérdésMegjelenítés(kérdés) {
+    
+}
